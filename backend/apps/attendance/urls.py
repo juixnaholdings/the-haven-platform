0ecdfcfacabd59cc559b0a694a61256cc9469e1f
Phinehas 'Von' Neumann <phinehasnewman@gmail.com>
2026-03-19 14:39:59 +0000

    feat(attendance): add admin apis urls and tests

--- backend/apps/attendance/urls.py
+++ backend/apps/attendance/urls.py
@@ -1,4 +1,29 @@
-﻿from django.urls import path
+from django.urls import path
 
-urlpatterns = []
+from apps.attendance.apis.admin import (
+    AttendanceSummaryAdminApi,
+    MemberAttendanceDetailAdminApi,
+    MemberAttendanceListCreateAdminApi,
+    ServiceEventDetailAdminApi,
+    ServiceEventListCreateAdminApi,
+)
 
+urlpatterns = [
+    path("", ServiceEventListCreateAdminApi.as_view(), name="service-event-list-create"),
+    path("<int:service_event_id>/", ServiceEventDetailAdminApi.as_view(), name="service-event-detail"),
+    path(
+        "<int:service_event_id>/summary/",
+        AttendanceSummaryAdminApi.as_view(),
+        name="attendance-summary-upsert",
+    ),
+    path(
+        "<int:service_event_id>/member-attendance/",
+        MemberAttendanceListCreateAdminApi.as_view(),
+        name="member-attendance-list-create",
+    ),
+    path(
+        "<int:service_event_id>/member-attendance/<int:member_attendance_id>/",
+        MemberAttendanceDetailAdminApi.as_view(),
+        name="member-attendance-detail",
+    ),
+]
