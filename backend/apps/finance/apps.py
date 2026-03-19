733a5e785976bcfe16d4cf6ad1f6d76c11e8d4ac
Phinehas 'Von' Neumann <phinehasnewman@gmail.com>
2026-03-19 16:41:08 +0000

    feat(finance): add fund account and ledger core models

--- backend/apps/finance/apps.py
+++ backend/apps/finance/apps.py
@@ -1,7 +1,7 @@
-﻿from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-class AuditConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.finance'
-    label = 'finance'
+class FinanceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.finance"
+    label = "finance"
